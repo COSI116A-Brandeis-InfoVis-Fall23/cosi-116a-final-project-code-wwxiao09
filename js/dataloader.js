3d22d6345fadd7b69ea9cb635fb0445bd509cd21
yueliulu <73615797+yueliulu@users.noreply.github.com>
2023-11-22 00:03:20 -0500

    keep only stations on main branch of green line

--- js/dataloader.js
+++ js/dataloader.js
@@ -1,13 +1,18 @@
 const csvFilePath = '../data/MBTA_Rail_Ridership.csv'; 
 const csv = require('csvtojson');
 const fs = require('fs');
+greenStopKeep = ["Lechmere", "Science Park", "North Station", "Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Prudential", "Symphony", "Hynes", "Kenmore"]
 
 csv()
   .fromFile(csvFilePath)
   .then((jsonObj) => {
     //console.log(jsonObj);
+    jsonObj = jsonObj.filter(obj => greenStopKeep.includes(obj['stop_name']));
 
-    jsonObj.forEach(obj => {
+    jsonObj.forEach((obj, idx) => {
+        console.log(obj);
+
+        delete obj['ObjectId'];
         delete obj['mode'];
         delete obj['season'];
         delete obj['day_type_id'];
@@ -15,6 +20,8 @@ csv()
         delete obj['total_ons'];
         delete obj['total_offs'];
         delete obj['number_service_days'];
+        
+        
       });
 
 
